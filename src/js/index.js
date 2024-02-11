2559f83617d1df730e1ecb42358525089204a91e
Uarles <121899351+Uarles@users.noreply.github.com>
2024-02-10 21:23:28 -0300

    7 commit

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,6 @@ const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas
 
 botao.addEventListener ("click", () => {
     
-    const botaoEstaAberto = elementoPlataformas.classList.contains("ativo");
+     elementoPlataformas.classList.toggle("ativo");
 
-    if(botaoEstaAberto) {
-        elementoPlataformas.classList.remove("ativo")
-    }else {
-        elementoPlataformas.classList.add("ativo");
-    }
 }); 
